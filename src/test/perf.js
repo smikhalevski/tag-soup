6b170f23bddad16b098b6d82b9acb1b1daf81350
Savva Mikhalevski <smikhalevski@gmail.com>
2020-09-10 22:47:25 +0300

    Better perf tests

--- src/test/perf.js
+++ src/test/perf.js
@@ -1,64 +1,72 @@
 const fs = require('fs');
 const path = require('path');
-const Benchmark = require('benchmark');
+const bench = require('nodemark');
 const htmlparser2 = require('htmlparser2');
-const {padEnd, padStart} = require('lodash');
+const sax = require('sax');
+const parse5 = require('parse5');
 const {createSaxParser, createXmlDomParser} = require('../../lib');
 const {createHtmlSaxParser, createHtmlDomParser} = require('../../lib/html');
 
-const html = fs.readFileSync(path.join(__dirname, './test.html'), {encoding: 'utf8'});
+const saxBenchDuration = 10000;
+const domBenchDuration = 10000;
 
-const suite = new Benchmark.Suite;
+const html = fs.readFileSync(path.join(__dirname, './test.html'), 'utf8');
 
-suite.on('complete', () => {
-  const benches = Array.from(suite);
+function round(v) {
+  return v.toFixed(1);
+}
 
-  for (const bench of benches) {
-    const ms = bench.stats.mean * 1000;
-    console.log(padEnd(bench.name, 40) + padStart(ms.toFixed(2), 8) + 'ms ± ' + bench.stats.rme.toFixed() + '%');
-  }
+console.log('SAX parser benchmark\n');
 
-  console.log('\nSAX XML  ratio = ' + (benches[0].stats.mean / benches[1].stats.mean).toFixed(2));
-  console.log('SAX HTML ratio = ' + (benches[0].stats.mean / benches[2].stats.mean).toFixed(2));
+const tagSoupSaxParser = createSaxParser({});
+const tagSoupSaxParserResult = bench(() => tagSoupSaxParser.commit(html), null, saxBenchDuration);
+console.log('createSaxParser     ', tagSoupSaxParserResult);
 
-  console.log('\nDOM XML  ratio = ' + (benches[3].stats.mean / benches[4].stats.mean).toFixed(2));
-  console.log('DOM HTML ratio = ' + (benches[3].stats.mean / benches[5].stats.mean).toFixed(2));
-});
+const tagSoupHtmlSaxParser = createHtmlSaxParser({});
+const tagSoupHtmlSaxParserResult = bench(() => tagSoupHtmlSaxParser.commit(html), null, saxBenchDuration);
+console.log('createHtmlSaxParser ', tagSoupHtmlSaxParserResult);
 
-// 0
 const htmlparser2SaxParser = new htmlparser2.Parser({});
-suite.add('SAX htmlparser2', () => {
-  htmlparser2SaxParser.write(html);
-});
+const htmlparser2SaxParserResult = bench(() => htmlparser2SaxParser.end(html), null, saxBenchDuration);
+console.log('htmlparser2         ', htmlparser2SaxParserResult);
 
-// 1
-const xmlSaxParser = createSaxParser({});
-suite.add('SAX XML  tag-soup (createSaxParser)', () => {
-  xmlSaxParser.commit(html);
-});
+const saxParser = sax.parser();
+const saxParserResult = bench(() => saxParser.write(html), null, saxBenchDuration);
+console.log('sax                 ', saxParserResult);
 
-// 2
-const htmlSaxParser = createHtmlSaxParser({});
-suite.add('SAX HTML tag-soup (createHtmlSaxParser)', () => {
-  htmlSaxParser.commit(html);
-});
+console.log(`
+htmlparser2
+  ${round(htmlparser2SaxParserResult.mean / tagSoupSaxParserResult.mean)}✕ slower than createSaxParser
+  ${round(htmlparser2SaxParserResult.mean / tagSoupHtmlSaxParserResult.mean)}✕ slower than createHtmlSaxParser
 
-// 3
+sax
+  ${round(saxParserResult.mean / tagSoupSaxParserResult.mean)}✕ slower than createSaxParser
+  ${round(saxParserResult.mean / tagSoupHtmlSaxParserResult.mean)}✕ slower than createHtmlSaxParser
+`);
+
+console.log('\nDOM parser benchmark\n');
+
+const tagSoupXmlDomParser = createXmlDomParser({});
+const tagSoupXmlDomResult = bench(() => tagSoupXmlDomParser.commit(html), null, domBenchDuration);
+console.log('createXmlDomParser  ', tagSoupXmlDomResult);
+
+const tagSoupHtmlDomParser = createHtmlDomParser({});
+const tagSoupHtmlDomParserResult = bench(() => tagSoupHtmlDomParser.commit(html), null, domBenchDuration);
+console.log('createHtmlDomParser ', tagSoupHtmlDomParserResult);
+
 const htmlparser2DomParser = new htmlparser2.Parser(new htmlparser2.DomHandler(() => null));
-suite.add('DOM htmlparser2', () => {
-  htmlparser2DomParser.write(html);
-});
+const htmlparser2DomParserResult = bench(() => htmlparser2DomParser.end(html), null, domBenchDuration);
+console.log('htmlparser2         ', htmlparser2DomParserResult);
 
-// 4
-const xmlDomParser = createXmlDomParser();
-suite.add('DOM XML  tag-soup (createXmlDomParser)', () => {
-  xmlDomParser.commit(html);
-});
+const parse5ParserResult = bench(() => parse5.parse(html), null, domBenchDuration);
+console.log('parse5              ', parse5ParserResult);
 
-// 5
-const htmlDomParser = createHtmlDomParser();
-suite.add('DOM HTML tag-soup (createHtmlDomParser)', () => {
-  htmlDomParser.commit(html);
-});
+console.log(`
+htmlparser2
+  ${round(htmlparser2DomParserResult.mean / tagSoupXmlDomResult.mean)}✕ slower than createXmlDomParser
+  ${round(htmlparser2DomParserResult.mean / tagSoupHtmlDomParserResult.mean)}✕ slower than createHtmlDomParser
 
-suite.run();
+parse5
+  ${round(parse5ParserResult.mean / tagSoupXmlDomResult.mean)}✕ slower than createXmlDomParser
+  ${round(parse5ParserResult.mean / tagSoupHtmlDomParserResult.mean)}✕ slower than createHtmlDomParser
+`);
